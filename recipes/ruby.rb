83ed3c0e32232bfc38da3ddcfe6e9e053b48c42f
Daniel <bassrock@users.noreply.github.com>
2016-07-21 22:42:03 -0700

    Fixing up user

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -7,16 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
+release_user = node[:deploy]["#{app[:shortname]}"][:release_user]
+release_group = node[:deploy]["#{app[:shortname]}"][:release_group]
+
+
 script 'Vagrant Use Ruby 2.1.6' do
   interpreter 'bash'
   cwd '/vagrant'
-  user 'vagrant'
-  environment  'HOME' => '/home/vagrant'
+  user release_user
+  environment  'HOME' => "/home/#{release_user}"
   code <<-EOH
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     curl -sSL https://get.rvm.io | bash -s stable
     source ~/.rvm/scripts/rvm
-    usermod -a -G rvm vagrant
+    usermod -a -G rvm #{release_user}
     rvm install ruby-2.1.6 || true
     rvm --default use 2.1.6
     curl -sSL https://rvm.io/mpapis.asc | gpg --import -
@@ -27,6 +31,6 @@ script 'Vagrant Use Ruby 2.1.6' do
 end
 
 ruby_block 'set new ruby for chef run' do
-  block { ENV['PATH'] = "/home/vagrant/.rvm/gems/ruby-2.1.6/bin:/home/vagrant/.rvm/gems/ruby-2.1.6@global/bin:/home/vagrant/.rvm/rubies/ruby-2.1.6/bin:#{ENV['PATH']}:/home/vagrant/.rvm/bin" }
+  block { ENV['PATH'] = "/home/#{release_user}/.rvm/gems/ruby-2.1.6/bin:/home/#{release_user}/.rvm/gems/ruby-2.1.6@global/bin:/home/#{release_user}/.rvm/rubies/ruby-2.1.6/bin:#{ENV['PATH']}:/home/#{release_user}/.rvm/bin" }
   not_if { ENV['PATH'].include?('rvm') }
 end
