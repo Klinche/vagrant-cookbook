c050eb9c86ab3ebcc3928ace5c21a7e53570a420
Daniel <bassrock@users.noreply.github.com>
2016-11-22 00:10:02 -0800

    Update ruby.rb

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -17,7 +17,7 @@ release_group = node[:deploy]["#{app[:shortname]}"][:release_group]
 script 'Vagrant Use Ruby 2.1.6' do
   interpreter 'bash'
   cwd '/vagrant'
-  user root
+  user 'root'
   environment  'HOME' => "/home/#{release_user}"
   code <<-EOH
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
