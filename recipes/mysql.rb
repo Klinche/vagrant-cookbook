93aa91ee48a9efbf73d73a5f5ad0bb2e1af3ab94
Daniel <bassrock@users.noreply.github.com>
2016-12-15 22:28:32 -0800

    Try at a redo here.

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -7,42 +7,13 @@ search('aws_opsworks_app', 'deploy:true').each do |app|
   name = node[:deploy][(app[:shortname]).to_s][:database][:database_name]
   host = node[:deploy][(app[:shortname]).to_s][:database][:database_host]
 
-  mysql_service 'default' do
-    action [:stop]
-  end
-
-  versionM = '5.6'
-  versionM = '5.7' if node['platform'] == 'ubuntu' && node['platform_version'] == '16.04'
-  
-  packageName = "mysql-server"
-  packageName = 'mysql-server-5.7' if node['platform'] == 'ubuntu' && node['platform_version'] == '16.04'
-  
-  packageVersion = nil
-  #packageVersion = '5.7.13-0ubuntu0.16.04.2' if node['platform'] == 'ubuntu' && node['platform_version'] == '16.04'
-
-
   # master
   mysql_service "default" do
     port port
-    version version
+    version '5.7'
     bind_address '0.0.0.0'
-    package_name packageName
-    package_version packageVersion
     initial_root_password root_pass
-    action [:create]
-  end
-  
-  #The package installs a version of mysql and auto starts it which conflicts with us.
-  execute 'stop old mysql' do
-    command "/bin/systemctl --system stop mysql > /dev/null 2> /dev/null"
-  end
-  
-  execute 'delete old mysql' do
-    command "systemctl disable mysql > /dev/null 2> /dev/null"
-  end
-  
-  mysql_service "default" do
-    action [:start]     
+    action [:create, :start]
   end
   
   execute 'drop database' do
